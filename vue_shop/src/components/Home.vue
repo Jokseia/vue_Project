e42c8d2f27a7848c06f4ccc4575d61a543d95845
Jokseia <2426733691@qq.com>
2020-03-18 23:34:27 +0800

    backgroud-color

--- vue_shop/src/components/Home.vue
+++ vue_shop/src/components/Home.vue
@@ -1,15 +1,43 @@
 <template>
-  <div>
-    Home组件
-  </div>
+<!-- 头部 -->
+  <el-container class="home-container">
+  <el-header>Header<el-button type="info" @click="logout">退出</el-button></el-header>
+  <!-- 页面主体区域 -->
+  <el-container>
+    <!-- 侧边栏 -->
+    <el-aside width="200px">Aside</el-aside>
+    <!-- 右侧内容主体 -->
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    logout() {
+      //清空 token 返回登录页面
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
+.home-container {
+  height:100%;
+}
+.el-header {
+  background-color: #373d41;
+}
+
+.el-aside {
+  background-color: #333744;
+}
+
+.el-main {
+  background-color: #eaedf1;
+}
 
 </style>
